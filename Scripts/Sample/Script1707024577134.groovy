f8b4f99628163c964d364c6f86b8bd2a56db6722
hp <hp@LAPTOP-IRDI9Q78>
2024-02-04 16:01:25 +0530

    update

--- Scripts/Sample/Script1707024577134.groovy
+++ Scripts/Sample/Script1707024577134.groovy
@@ -19,8 +19,6 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
-WebUI.maximizeWindow()
-
 WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.click(findTestObject('Object Repository/Sample/Page_CURA Healthcare Service/a_Make Appointment'))
